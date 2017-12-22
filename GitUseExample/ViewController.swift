33bd1311377a14f1fa8df3c8e447701f1c505302
Paul Ingram <paulingram@Pauls-iMac.local>
2017-12-22 16:53:01 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Paul Ingram on 12/22/17.
+//  Copyright © 2017 Paul Ingram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
