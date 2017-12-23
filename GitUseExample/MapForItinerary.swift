0a14709b55a243710af428b312127c4d690eaf99
Paul Ingram <paulingram@Pauls-iMac.local>
2017-12-22 20:41:00 -0600

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Paul Ingram on 12/22/17.
+//  Copyright © 2017 Paul Ingram. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
